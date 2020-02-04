77d8d79635c6457fcff3add53709143050b22c52
Mitty <mitty.fun@gmail.com>
2020-02-04 23:09:48 +0800

    solving process in animation

--- ai/ai.js
+++ ai/ai.js
@@ -1,63 +1,86 @@
 function AI (game) {
+    const { setFlag, safePlace } = analytics(game)
 
-    let arr = game.getAllCos();
+    const id = setInterval(() => {
+        if (setFlag.length > 0) {
+            return setFlag.pop().isFlag = true
+        }
+        if (safePlace.length > 0) {
+            return game.open(safePlace.pop())
+        }
+        clearInterval(id)
+        if (game.status === 'playing') AI(game)
+    })
+}
+
+function analytics (game) {
+
+    let arr = game.getAllCos()
     
     arr.forEach(col => {
-        col.isFocus = false;
-        col.probability = undefined;
-    });
-
-    arr = arr.filter((col) => {
-        return !col.isOpen && !col.isFlag && game.getNeighbors(col).some(col => col.isOpen);
+        col.isFocus = false
+        col.probability = undefined
     })
 
-    arr.forEach(col => {
-        col.isFocus = true;
-        col.probability = 0;
-    });
+    arr = arr.filter((col) => {
+        return !col.isOpen && !col.isFlag && game.getNeighbors(col).some(col => col.isOpen)
+    })
 
-    let count = 0;
-    let nodes = 0;
+    if (arr.length === 0) return { setFlag: [], safePlace: [game.getAllCos()[0]] }
+
+    arr.forEach(col => {
+        col.isFocus = true
+        col.probability = 0
+    })
+
+    let count = 0
+    let nodes = 0
 
     function resolve (arr, deep) {
 
-        nodes++;
+        nodes++
 
         if (deep >= arr.length) {
-            arr.filter(col => col.pretend === 'bomb').forEach(col => col.probability++);
-            count++;
-            return;
+            arr.filter(col => col.pretend === 'bomb').forEach(col => col.probability++)
+            count++
+            return
         }
 
-        const col = arr[deep];
+        const col = arr[deep]
 
-        col.pretend = 'save';
-        if (isValid(col, game)) resolve(arr, deep + 1);
+        col.pretend = 'save'
+        if (isValid(col, game)) resolve(arr, deep + 1)
 
-        col.pretend = 'bomb';
-        if (isValid(col, game)) resolve(arr, deep + 1);
+        col.pretend = 'bomb'
+        if (isValid(col, game)) resolve(arr, deep + 1)
 
-        
-
-        col.pretend = undefined;
+        col.pretend = undefined
     }
     
-    resolve(arr, 0);
+    resolve(arr, 0)
+
+    const setFlag = []
+    const safePlace = []
 
     arr.forEach((col) => {
-        if (col.probability === count) col.isFlag = true;
-        if (col.probability === 0) game.open(col);
-    });
+        if (col.probability === count) setFlag.push(col)
+        if (col.probability === 0) safePlace.push(col)
+        // if (col.probability === count) col.isFlag = true
+        // if (col.probability === 0) game.open(col)
+    })
 
     if (arr.some((col) => col.isOpen) === false) {
-        var col = arr.sort((col) => col.probability)[0];
-        game.open(col);
+        let col = arr.sort((col) => col.probability)[0]
+        // game.open(col)
+        safePlace.push(col)
     }
     
     arr.forEach((col) => {
-        col.probability = undefined;
-        col.isFocus = false;
+        col.probability = undefined
+        col.isFocus = false
     })
+
+    return { setFlag, safePlace }
 }
 
 
@@ -65,35 +88,33 @@ function AI (game) {
 function isValid (cell, game) {
 
     let judgers = game.getNeighbors(cell).filter((cell) => {
-        return cell.isOpen && cell.number > 0;
-    });
-
-    
+        return cell.isOpen && cell.number > 0
+    })
 
     for (let i=0; i<judgers.length; i++) {
-        let judger = judgers[i];
+        let judger = judgers[i]
 
-        let col;
-        let bombs = 0;
-        let saves = 0;
-        let unknow = 0;
-        let judgerNe = game.getNeighbors(judger);
+        let col
+        let bombs = 0
+        let saves = 0
+        let unknow = 0
+        let judgerNe = game.getNeighbors(judger)
         
         for (let i=0; i<judgerNe.length; i++) {
-            col = judgerNe[i];
+            col = judgerNe[i]
             if (col.isOpen || col.pretend === 'save') {
-                saves++;
+                saves++
             } else if (col.isFlag || col.pretend === 'bomb') {
-                bombs++;
+                bombs++
             } else {
-                unknow++;
+                unknow++
             }
         }
         if (bombs > judger.number || bombs + unknow < judger.number) {
-            return false;
+            return false
         }
     }
 
-    return true;
+    return true
 
 }
